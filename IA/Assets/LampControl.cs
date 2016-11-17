5154626cf5a94d1cbf659f2586a62e6222943ad9
Hugo Borel <Hugo Borel>
2016-11-17 17:12:23 +0100

    premiers essais

---
+++ IA/Assets/LampControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LampControl : MonoBehaviour {
+    Light spotlight;
+	// Use this for initialization
+	void Start () {
+        spotlight = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+            spotlight.enabled = !spotlight.enabled;
+	}
+}
---
+++ IA/Assets/Poursuite.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Poursuite : MonoBehaviour {
+
+    public GameObject joueur;
+    NavMeshAgent nm;
+
+	// Use this for initialization
+	void Start () {
+        nm = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nm.SetDestination(joueur.transform.position);
+	}
+}
