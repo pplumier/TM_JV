ea9585ad863b384839cf9606d675c2d843a904a6
pplumier <pplumier@enseirb-matmeca.fr>
2016-11-27 17:30:42 +0100

    début ajout porte

---
+++ Niveau/Assets/Bank Assets/SciFi_Door/Script/Door.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider obj)
+    {
+        var thedoor = GameObject.FindWithTag("SF_Door");
+        thedoor.GetComponent<Animation>().Play("open");
+    }
+
+    private void OnTriggerExit(Collider obj)
+    {
+        var thedoor = GameObject.FindWithTag("SF_Door");
+        thedoor.GetComponent<Animation>().Play("close");
+    }
+}
