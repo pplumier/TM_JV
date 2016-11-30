868af365f41ccd10efe6307688817b98b7730572
pplumier <pplumier@enseirb-matmeca.fr>
2016-11-30 11:47:14 +0100

    problème des portes fixé

---
+++ Niveau/Assets/Scripts/Door.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider obj)
+    {
+        GetComponent<Animation>().Play("open");
+    }
+
+    private void OnTriggerExit(Collider obj)
+    {
+        GetComponent<Animation>().Play("close");
+    }
+}
