1c85a4b8a7231a3e99ee7d26b7116007c1284228
pplumier <pplumier@enseirb-matmeca.fr>
2016-12-07 10:11:36 +0100

    ajout d'un répertoire pour fusion des deux parties

---
+++ Tout/Assets/Scripts/Door.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GetComponent<Animation>().Play("open");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GetComponent<Animation>().Play("close");
+        }
+    }
+}
