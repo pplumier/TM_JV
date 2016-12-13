a2be44fa379361f66ff8c3fe2f0be0ec18c68955
hborel <hugo.borel@wanadoo.fr>
2016-12-13 23:38:54 +0100

    Un peu de ménage + tentative pull object

--- Tout/Assets/Scripts/AddAlly.cs
+++ Tout/Assets/Scripts/AddAlly.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class AddAlly : MonoBehaviour {
 
+    public float minDistance = 5f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,13 +12,13 @@ public class AddAlly : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetButtonDown("Fire3"))
+        if(Input.GetButtonDown("TractAndAlly"))
         {
             Ray ray = new Ray(transform.position, transform.forward);
             RaycastHit hitInfo;
             Physics.Raycast(ray, out hitInfo);
             GameObject go = hitInfo.transform.gameObject;
-            if (go.tag == "Ally")
+            if (go.tag == "Ally" && Vector3.Magnitude(transform.position - go.transform.position) < minDistance)
             {
                 Follower f = go.GetComponent<Follower>();
                 f.activate = !f.activate;
---
+++ Tout/Assets/TractObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TractObject : MonoBehaviour {
+
+    public float minDistance = 10f;
+    Rigidbody r;
+    Vector3 lastVelocity;
+
+    // Use this for initialization
+    void Start () {
+        r = GetComponentInParent<Rigidbody>();
+        lastVelocity = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("TractAndAlly"))
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hitInfo;
+            Physics.Raycast(ray, out hitInfo);
+            GameObject go = hitInfo.transform.gameObject;
+            Debug.Log(go.tag);
+            if (go.tag == "Tractable" && Vector3.Magnitude(transform.position - go.transform.position) < minDistance)
+            {
+                Debug.Log("tract");
+                go.GetComponent<Rigidbody>().AddForce((r.velocity - lastVelocity) * 1000, ForceMode.Impulse);
+            }
+            lastVelocity = r.velocity;
+        }
+    }
+}
