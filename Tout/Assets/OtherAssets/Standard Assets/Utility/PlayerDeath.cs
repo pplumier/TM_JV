007719b64ff9c5094218d303fa5dc421387d24b4
Hugo Borel <Hugo Borel>
2017-02-02 14:31:54 +0100

    Nettoyage des conneries à pierreé

---
+++ Tout/Assets/OtherAssets/Standard Assets/Utility/PlayerDeath.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerDeath : MonoBehaviour {
+
+    bool dead;
+    float ToD;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (dead && Time.realtimeSinceStartup - ToD > 5f)
+        {
+            int scene = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        }   
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("hi");
+        if(collision.gameObject.tag == "IA" && !dead)
+        {
+            dead = true;
+            ToD = Time.realtimeSinceStartup;
+            GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = false;
+            Debug.Log("Player Died");
+        }     
+    }
+}
